4e8ab0b6cf40dcd34e40df393ce6ee9778ef3246
xieyong <941216328xyXY>
2018-08-28 19:49:51 +0800

    响应式编程router例子

--- jason-webflux/src/main/java/com/jason/webflux/router/hander/CityHandler2.java
+++ jason-webflux/src/main/java/com/jason/webflux/router/hander/CityHandler2.java
@@ -7,8 +7,11 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
- * @auther: xieyong
+ * @author: xieyong
  * @date: 2018/8/28 10:01
  * @Description:
  */
@@ -17,5 +20,14 @@ public class CityHandler2 {
     public Mono<ServerResponse> helloCity(ServerRequest request) {
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
                 .body(BodyInserters.fromObject("Hello, City!"));
+    }
+    public Mono<ServerResponse> timeFunction() {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(
+                Mono.just("Now is " + new SimpleDateFormat("HH:mm:ss").format(new Date())), String.class);
+    }
+
+    public Mono<ServerResponse> dateFunction() {
+        return  ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(
+                Mono.just("Today is " + new SimpleDateFormat("yyyy-MM-dd").format(new Date())), String.class);
     }
 }
--- jason-webflux/src/main/java/com/jason/webflux/router/router/CityRouter.java
+++ jason-webflux/src/main/java/com/jason/webflux/router/router/CityRouter.java
@@ -1,13 +1,15 @@
 package com.jason.webflux.router.router;
 import com.jason.webflux.router.hander.CityHandler2;
+import com.jason.webflux.router.hander.TimeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 /**
  * @auther: xieyong
  * @date: 2018/8/28 10:02
@@ -17,11 +19,12 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class CityRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> routeCity(CityHandler2 cityHandler) {
-        return RouterFunctions
-                .route(RequestPredicates.GET("/hello")
-                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
-                        cityHandler::helloCity);
+    public RouterFunction<ServerResponse> routeCity(CityHandler2 cityHandler,TimeHandler timeHandler) {
+        return route(GET("/hello").
+                        and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        cityHandler::helloCity)
+                .andRoute(GET("/time"),timeHandler::getTime)
+                .andRoute(GET("/date"),timeHandler::getDate)
+                .andRoute(GET("/times"),timeHandler::sendTimePerSec);
     }
-
 }
